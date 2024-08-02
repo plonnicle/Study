bbb0538d451fce46b27af923269d6d7e305b167a
Desmond Darko <51133264+plonnicle@users.noreply.github.com>
2024-08-02 19:25:18 -0400

    Create ReverseInParentheses.java

---
+++ Queue/ReverseInParentheses.java
@@ -0,0 +1,30 @@
+//Summary: Take an input string, and for the content inside of a pair of parentheses, reverse the substring. Return the final string. 
+//Approach: Recursive. One pointer. Iterate through the string, and if a character
+//Solution:
+String solution(String s) {
+        int n = s.length();
+        int[] d = new int[n];
+        Deque<Integer> stk = new ArrayDeque<>();
+        for (int i = 0; i < n; ++i) {
+            if (s.charAt(i) == '(') {
+                stk.push(i);
+            } else if (s.charAt(i) == ')') {
+                int j = stk.pop();
+                d[i] = j;
+                d[j] = i;
+            }
+        }
+        
+        StringBuilder ans = new StringBuilder();
+        int i = 0, x = 1;
+        while (i < n) {
+            if (s.charAt(i) == '(' || s.charAt(i) == ')') {
+                i = d[i];
+                x = -x;
+            } else {
+                ans.append(s.charAt(i));
+            }
+            i += x;
+        }
+        return ans.toString();
+}
