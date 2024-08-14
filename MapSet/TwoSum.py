e847840a26aba83f540c8c9e6575a09b3cb459ef
Desmond Darko <51133264+plonnicle@users.noreply.github.com>
2024-08-13 20:38:08 -0400

    Create TwoSum.py

---
+++ MapSet/TwoSum.py
@@ -0,0 +1,12 @@
+#Summary: Input: Array of integers nums, integer target. Return the [match1, match2] that equals target.
+#Approach: For loop through nums. Compare the current value to the next values and see if it matches. If so, return the pair. Hash Map. Two Pointer.
+#Solution:
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        numMap = {}
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in numMap:
+                return [numMap[complement], i]
+            numMap[num] = i
+        return []
