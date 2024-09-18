77241c15f90b8e09774a54ffb94dc6ff983565ad
Desmond Darko <51133264+plonnicle@users.noreply.github.com>
2024-09-18 13:57:27 -0400

    Create IntersectionArrays.py

---
+++ ArrayString/IntersectionArrays.py
@@ -0,0 +1,23 @@
+#Summary: From integer arrays nums1 and nums2, return the common elements between both in any order.
+#Approach: Iterate through nums1 and check to see if nums2 has any equal values (nested for loop). If so, add them to the final array. Return the final array.
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        mainArr = []
+        checkerArr = []
+        finalArr = []
+        
+        if len(nums1) >= len(nums2):
+            mainArr = nums1
+            checkerArr = nums2
+        else:
+            mainArr = nums2
+            checkerArr = nums1
+            
+        for i in range(len(mainArr)):
+            for j in range(len(checkerArr)):
+                if mainArr[i] == checkerArr[j] and checkerArr[j] not in finalArr:
+                    finalArr.append(checkerArr[j])
+                else:
+                    continue
+        
+        return finalArr
